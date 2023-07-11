65d8bcb3f768d82b37b67e1d13ec51af3eb95fb2
davilivob <david.iii.chiu@gmail.com>
2023-07-11 14:06:42 +0800

    Add dependencies: ws

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,29 @@
 import screenshot from 'screenshot-desktop';
+import WebSocket from 'ws';
+import 'dotenv/config';
+import config from './config.js';
+
+const WS_URL = process.env.WS_URL as string;
 
 const img_root = './images';
 let n = 0;
 
+let n: number = 0;
 
-setInterval(() => {
-	screenshot({ filename: `${img_root}/${n}.jpg` }).then((imgPath) => {
-		console.log(imgPath);
-		n++;
-	});
-}, 10000)
+const ws = new WebSocket(WS_URL);
+ws.on('error', (err) => {
+	console.log(err);
+});
+ws.on('open', () => {
+	console.log('connected');
+});
+ws.on('message', (data) => {
+	console.log(data);
+});
+
+// setInterval(() => {
+// 	screenshot({ filename: `${IMG_PATH_ROOT}/${n}.jpg` }).then((imgPath) => {
+// 		console.log(imgPath);
+// 		n++;
+// 	});
+// }, 10000);

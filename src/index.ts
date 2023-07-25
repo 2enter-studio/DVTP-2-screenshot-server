56574f49de904f80cf0dff2f18167418f215c66b
davilivob <david.iii.chiu@gmail.com>
2023-07-25 13:03:11 +0800

    Detect exhibition location

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import WebSocket from 'ws';
-import 'dotenv/config';
-import config from './config.js';
-import { ScreenshotTaker } from './lib/screenshot.js';
-import * as db from './lib/database.js';
-import validator from 'validator';
-import { crop } from './lib/crop-img.js';
+import WebSocket from "ws";
+import "dotenv/config";
+import config from "./config.js";
+import { ScreenshotTaker } from "./lib/screenshot.js";
+import * as db from "./lib/database.js";
+import validator from "validator";
+import { crop } from "./lib/crop-img.js";
 
 const WS_URL = process.env.WS_URL as string;
 
@@ -15,27 +15,35 @@ await db.connect_to_db().then((res) => res.text().then(console.log));
 
 let ws = new WebSocket(WS_URL);
 
+const exhibition_location = process.env.EXHIBITION_LOCATION;
+console.log(exhibition_location);
+
 const ws_connect = () => {
 	ws = new WebSocket(WS_URL);
 
-	ws.on('error', (err: unknown) => {
+	ws.on("error", (err: unknown) => {
 		console.log(err);
 	});
 
-	ws.on('open', () => {
-		ws.send('Screenshot server connected');
+	ws.on("open", () => {
+		ws.send("Screenshot server connected");
 	});
 
-	ws.on('close', () => {
-		console.log('Websocket connection broke, trying to reconnecxt...');
+	ws.on("close", () => {
+		console.log("Websocket connection broke, trying to reconnecxt...");
 		ws_connect();
 	});
 
-	ws.on('message', async (data) => {
+	ws.on("message", async (data) => {
 		if (validator.isJSON(data.toString())) {
 			const parsed_data = JSON.parse(data.toString());
 			if (!parsed_data._id) {
-				console.log('Invalid data received');
+				console.log("Invalid data received");
+				return;
+			}
+
+			if (parsed_data.location !== exhibition_location) {
+				console.log(`Invalid location: ${parsed_data.location}`);
 				return;
 			}
 			const answer_id = parsed_data._id;
@@ -43,14 +51,11 @@ const ws_connect = () => {
 			await screenshot_taker.take_shot(answer_id).then(async (img_path) => {
 				await new Promise((resolve) => setTimeout(resolve, 2000));
 				await crop(img_path).then(async (data) => {
-					console.log('done!');
-					console.log(data);
+					console.log("done!");
+					// console.log(data);
 					await db.upload_cropped_screenshot(answer_id, data).then((res) => {
 						res.text().then(console.log);
 					});
-					// db.upload_screenshot(answer_id, img_path.replace('.jpg', '_cropped.jpg')).then((res) =>
-					// 	res.text().then(console.log)
-					// );
 				});
 			});
 		} else {

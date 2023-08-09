a0b4c340ce39371bef7c32264fb8ba09ae9962de
davilivob <david.iii.chiu@gmail.com>
2023-08-10 03:16:05 +0800

    Update POST request URL

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -35,7 +35,7 @@ export const upload_screenshot = async (id: string, img_path: string) => {
 	return new Response(`Screenshot for ${id} uploaded`, { status: 200 });
 };
 export const upload_cropped_screenshot = async (id: string, img_buffer: Buffer) => {
-	await fetch('http://localhost:5173/api/screenshot', {
+	await fetch('https://dvtp2.2enter.art/api/screenshot', {
 		method: 'POST',
 		body: JSON.stringify({ id, screenshot: img_buffer })
 	});
